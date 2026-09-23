514604e114286d25d6f2f0da0f13da0fc751fc0e
hsoft7 <harisoftwares7@gmail.com>
2026-09-23 16:07:00 +0530

    asas

--- src/pages/Editor/GenralEditPage.jsx
+++ src/pages/Editor/GenralEditPage.jsx
@@ -1631,7 +1631,10 @@ function GeneralEditPage({
   );
 
   const [imgTraining] = useImage(selll?.ShowCaseForm || "", "anonymous");
-  const [imgMeeting] = useImage(selll?.meeting_badge_image || "", "anonymous");
+  const [imgMeeting] = useImage(
+    selected?.meeting_badge_image || "",
+    "anonymous",
+  );
   const [tankyoubadge] = useImage(savedDesignation?.profileimage, "anonymous");
   const [rankbadge] = useImage(`${selected?.rankNameImageUrl}`, "anonymous");
   const [tripbadge] = useImage(`${selected?.trip_batch_image}`, "anonymous");
